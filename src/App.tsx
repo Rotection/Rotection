9526887c90616f2a1ef577c0b92a96c9fbf96846
karimico2013 <karimico2013@gmail.com>
2025-11-29 20:27:38 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,30 @@ import { motion, AnimatePresence } from 'motion/react';
 
 // Mock data for verified games
 const verifiedGames = [
-  {
+      {
     id: '1',
+    name: '[ METALLIX ] Speedsters Sandbox',
+    developer: 'Speedsters Sandbox',
+    thumbnail: 'https://tr.rbxcdn.com/180DAY-bf6e00c4f61b35b36ea92d828e4c3232/768/432/Image/Webp/noFilter',
+    safetyScore: 90,
+    ageRating: '5+',
+    ratings: {
+      honesty: 5,
+      safety: 4,
+      fairness: 4,
+      ageAppropriate: 5
+    },
+    totalRatings: 'Staff',
+    verified: true,
+    description: 'Speedsters Sandbox.
+
+Find universal skins around the map from diffrent games/animes/shows etc.
+race other players and have fun!',
+    category: 'Sandbox'
+  },
+  
+  {
+    id: '2',
     name: 'Pilgrammed',
     developer: 'Phexonia Studios',
     thumbnail: 'https://tr.rbxcdn.com/180DAY-1985fb1fa6534213463d7814f3958298/768/432/Image/Webp/noFilter',
@@ -24,14 +46,14 @@ const verifiedGames = [
       fairness: 3,
       ageAppropriate: 4
     },
-    totalRatings: '1',
+    totalRatings: 1,
     verified: false,
     description: 'Pilgrammed is an open-world RPG game. You can craft guns and armor.',
     category: 'Party & Casual'
   },
 
     {
-    id: '2',
+    id: '3',
     name: 'Prophecy Battle Arena',
     developer: '@GoldenObscurity',
     thumbnail: 'https://tr.rbxcdn.com/180DAY-f686b29c5acc0a09de9c8edb5eaddfd6/768/432/Image/Webp/noFilter',
@@ -43,7 +65,7 @@ const verifiedGames = [
       fairness: 4,
       ageAppropriate: 4
     },
-    totalRatings: '1',
+    totalRatings: 1,
     verified: true,
     description: 'Prophecy is a PvP round based battleground game. Choose from up to 12 different custom characters, and fight to the death!',
     category: 'Action RPG'
